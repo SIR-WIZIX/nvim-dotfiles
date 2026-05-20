b1df4d5ba6ed36a001932772da94adb2e3be7f8c
SIR-WIZIX <egorbezbabchenko@gmail.com>
2026-05-20 15:15:10 +0300

    massive restructure

---
+++ README.md
@@ -0,0 +1,14 @@
+# nvim-dotfiles
+
+I have messed up some things cuz there were a lot of time without proper updates
+Now i have to restructure entire repository to continue using neovim after all updates
+
+---
+
+## TODO
+
+- [x] ~configure lazyvim properly~
+- [ ] set up keybinds
+- [ ] reconfigure all plugins
+- [ ] drop unused plugins
+- [ ] add local running ai code suggestions
---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        mode = "background",
+        names = true,
+	RRGGBBAA = true,        -- #RRGGBBAA hex codes
+	rgb_fn   = true,        -- CSS rgb() and rgba() functions
+	hsl_fn   = true,        -- CSS hsl() and hsla() functions
+	css      = true,        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	css_fn   = true,        -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      },
+    },
+  },
+}
